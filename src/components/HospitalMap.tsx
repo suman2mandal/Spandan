e64aa615a2f6336307a826fb71b2c8cce56ffb2d
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2025-07-04 08:05:14 +0530

    Map icon issue fixed

--- src/components/HospitalMap.tsx
+++ src/components/HospitalMap.tsx
@@ -4,16 +4,14 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+const customIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
 const hospitalCoords: [number, number] = [22.4922, 88.4081];
@@ -31,7 +29,7 @@ export default function HospitalMap() {
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={hospitalCoords}>
+        <Marker position={hospitalCoords} icon={customIcon}>
           <Popup>
             Veterinary Hospital <br /> 22°29&apos;31.9&quot;N, 88°24&apos;29.0&quot;E
           </Popup>
